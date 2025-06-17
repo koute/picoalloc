1fdd70b2c664bba64ba00366eab9d6935f1c7f5b
Jan Bujak <jan@parity.io>
2025-06-17 08:29:22 +0000

    Move `GlobalAlloc` impl to allocator.rs

--- src/allocator.rs
+++ src/allocator.rs
@@ -954,3 +954,25 @@ impl<E: Env> Allocator<E> {
         unsafe { &*pointer.byte_sub(HEADER_SIZE.bytes() as usize).cast::<ChunkHeader>() }
     }
 }
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+#[cfg(feature = "alloc")]
+unsafe impl<E: crate::Env> alloc::alloc::GlobalAlloc for crate::Mutex<Allocator<E>> {
+    unsafe fn alloc(&self, layout: alloc::alloc::Layout) -> *mut u8 {
+        let Some(align) = Size::from_bytes_usize(layout.align()) else {
+            return core::ptr::null_mut();
+        };
+
+        let Some(size) = Size::from_bytes_usize(layout.size()) else {
+            return core::ptr::null_mut();
+        };
+
+        self.lock().alloc(align, size).unwrap_or(core::ptr::null_mut())
+    }
+
+    unsafe fn dealloc(&self, pointer: *mut u8, _layout: alloc::alloc::Layout) {
+        self.lock().free(pointer);
+    }
+}
